bc91bef1fd7e38e7ec33bf3c3fd9198134d6f54b
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-09-16 12:49:22 -0300

    update

--- dep/systemd/scripts/firewall/c.sh
+++ dep/systemd/scripts/firewall/c.sh
@@ -3,13 +3,13 @@
 # Close on any error (Optional)
 #set -e
 
-#LAN0=enp1s0
+#NIC0=enp1s0
 
 vm859224() {
     3390() {
         #RDP- MSTSC
         # DNAT Rules
-        nft add rule inet firelux prerouting iifname "$LAN0" ip protocol tcp tcp dport 3390 dnat to 10.0.10.2:3389
+        nft add rule inet firelux prerouting iifname "$NIC0" ip protocol tcp tcp dport 3390 dnat to 10.0.10.2:3389
 
         # Forward Rules
         nft add rule inet firelux forward ip protocol tcp tcp dport 3390 accept
