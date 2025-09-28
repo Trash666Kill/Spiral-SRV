7a11e9dc58a80a24337d261e2c8a4b5590191572
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-09-28 09:21:52 -0300

    update

--- vnc-server.sh
+++ vnc-server.sh
@@ -1,15 +1,40 @@
-apt-get install -y fluxbox x11vnc websockify novnc xvfb
-vim /usr/local/bin/start-vnc.sh
-{
 #!/bin/bash
 
-# Inicia o servidor X11 (fluxbox)
-Xvfb :99 -screen 0 1024x768x24 &
+vnc_server() {
+    printf "\e[32m*\e[0m SETTING UP DESKTOP ENVIRONMENT AND VNC SERVER\n"
 
-# Inicia o servidor VNC
-x11vnc -display :99 -nohttpd -noxdamage -forever -shared -bg -passwd mypassword &
+    # Default user
+    TARGET_USER=$(grep 1001 /etc/passwd | cut -f 1 -d ":")
 
-# Inicia o noVNC
-websockify --web /usr/share/novnc 8080 localhost:5900
+    # Installing requirements
+    apt-get install --no-install-recommends xorg openbox tigervnc-standalone-server tigervnc-common tigervnc-tools novnc
+
+    # Building the environment
+    su - "$TARGET_USER" -c "mkdir -p /home/$TARGET_USER/.config/tigervnc"
+
+    su - "$TARGET_USER" -c "printf '#!/bin/sh
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+exec /bin/sh /etc/X11/xinit/xinitrc' > /home/$TARGET_USER/.config/tigervnc/xstartup && chmod +x /home/$TARGET_USER/.config/tigervnc/xstartup"
+
+    printf '[Unit]
+Description=Start VNC Server and noVNC Proxy
+After=network.target
+
+[Service]
+Type=simple
+User=%s
+Group=%s
+ExecStartPre=/usr/bin/vncserver -AcceptSetDesktopSize :1
+ExecStart=/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901
+ExecStop=/usr/bin/vncserver -kill :1
+Environment=DISPLAY=:1
+
+[Install]
+WantedBy=multi-user.target' "$TARGET_USER" "$TARGET_USER" > /etc/systemd/system/novnc.service
 }
-chmod +x /usr/local/bin/start-vnc.sh
+
+
+
+
+
