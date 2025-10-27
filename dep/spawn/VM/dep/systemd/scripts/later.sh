1e919ffbe9561d26a9379da6dd2b17c15ba91c41
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-27 20:28:02 -0300

    update

--- dep/spawn/VM/dep/systemd/scripts/later.sh
+++ dep/spawn/VM/dep/systemd/scripts/later.sh
@@ -5,8 +5,28 @@ unset HISTFILE
 
 NIC0=ens2
 
-# Obtendo o hostname da máquina
-HOSTNAME=$(hostname)
+hostname() {
+    # Install the required packages
+    apt-get -y install uuid uuid-runtime > /dev/null 2>&1
+
+    # Generates a new hostname based on the chassis type and a random value
+    HOSTNAME="vm$(shuf -i 100000-999999 -n 1)"
+
+    printf "\e[32m*\e[0m GENERATED HOSTNAME: \033[32m%s\033[0m\n" "$HOSTNAME"
+
+    # Remove the /etc/hostname file and write the new hostname
+    rm /etc/hostname
+    printf "$HOSTNAME" > /etc/hostname
+
+    # Remove the /etc/hosts file and writes the new hosts entries
+    rm /etc/hosts
+    printf "127.0.0.1       localhost
+127.0.1.1       "$HOSTNAME"
+
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters" > /etc/hosts
+}
 
 network() {
     # Restart the systemd-resolved service to ensure DNS resolution is active
@@ -58,6 +78,7 @@ finish() {
 }
 
 main() {
+    hostname
     network
     passwords
     baseboard
