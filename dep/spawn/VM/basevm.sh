27d0c63e6530876c9bdb7dc2db43f52a104dfe69
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-27 20:28:47 -0300

    update

--- dep/spawn/VM/basevm.sh
+++ dep/spawn/VM/basevm.sh
@@ -159,29 +159,6 @@ interface() {
     fi
 }
 
-hostname() {
-    # Install the required packages
-    apt-get -y install uuid uuid-runtime > /dev/null 2>&1
-
-    # Generates a new hostname based on the chassis type and a random value
-    HOSTNAME="vm$(shuf -i 100000-999999 -n 1)"
-
-    printf "\e[32m*\e[0m GENERATED HOSTNAME: \033[32m%s\033[0m\n" "$HOSTNAME"
-
-    # Remove the /etc/hostname file and write the new hostname
-    rm /etc/hostname
-    printf "$HOSTNAME" > /etc/hostname
-
-    # Remove the /etc/hosts file and writes the new hosts entries
-    rm /etc/hosts
-    printf "127.0.0.1       localhost
-127.0.1.1       "$HOSTNAME"
-
-::1     localhost ip6-localhost ip6-loopback
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters" > /etc/hosts
-}
-
 target_user() {
     # Install the sudo package
     apt-get -y install sudo > /dev/null 2>&1
@@ -480,7 +457,6 @@ main() {
     update
     cloud_kernel
     interface
-    hostname
     target_user
     packages
     directories
