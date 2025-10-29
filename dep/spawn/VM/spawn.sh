fa512cb7d262e39c89e72cd69ad7abc926c4036f
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-29 02:01:43 -0300

    update

--- dep/spawn/VM/spawn.sh
+++ dep/spawn/VM/spawn.sh
@@ -48,7 +48,7 @@ basevm() {
     # Check if base virtual machine already exists
     output=$(eval "$VM_MANAGER" list)
 
-    if ! echo "$output" | grep -q "$BASE_VM_NAME"; then
+    if ! echo "$output" | grep -qE "${BASE_VM_NAME}[[:space:]]"; then
         printf "\e[33m*\e[0m ATTENTION: THE BASE VIRTUAL MACHINE \033[32m%s\033[0m DOES NOT EXIST, WAIT...\n" "$BASE_VM_NAME"
     fi
 
--- temp.sh
+++ temp.sh
@@ -1,3 +1,10 @@
-if ! echo "$output" | grep -q "$BASE_VM_NAME"; then
-    printf "\e[33m*\e[0m ATTENTION: THE BASE VIRTUAL MACHINE \033[32m%s\033[0m DOES NOT EXIST, WAIT...\n" "$BASE_VM_NAME"
-fi
+# Usamos grep -E para regex estendida.
+    # A regex "${BASE_VM_NAME}[[:space:]]" procura por "SpiralVM"
+    # seguido por pelo menos um espaço ou tab.
+    #
+    # - Isso corresponderá a: "SpiralVM     [STOPPED]"
+    # - Isso NÃO corresponderá a: "SpiralVM-Pre [STOPPED]"
+    
+    if ! echo "$output" | grep -qE "${BASE_VM_NAME}[[:space:]]"; then
+        printf "\e[33m*\e[0m ATTENTION: THE BASE VIRTUAL MACHINE \033[32m%s\033[0m DOES NOT EXIST, WAIT...\n" "$BASE_VM_NAME"
+    fi
