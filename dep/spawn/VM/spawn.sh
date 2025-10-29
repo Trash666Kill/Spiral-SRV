98d20b6268749ce92e0a28e9b8ec271e6ca92332
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-29 01:45:28 -0300

    update

--- dep/spawn/VM/spawn.sh
+++ dep/spawn/VM/spawn.sh
@@ -28,9 +28,9 @@ NEW_VM_NAME="vm$(shuf -i 100000-999999 -n 1)"
 BASE_VM="${BASE_VM_FILES[1]}"
 
 basevm() {
+    # Checks if the files needed to create the base virtual machine exist 
     local missing_files=0 # Variable to count missing files
 
-    # Checks if the files needed to create the base virtual machine exist
     for file in "${BASE_VM_FILES[@]}"; do
         if [[ ! -f "$file" ]]; then
             printf "\e[31m*\e[0m ERROR: REQUIRED FILE DOES NOT EXIST: \033[32m%s\033[0m\n" "$file"
@@ -44,6 +44,12 @@ basevm() {
         exit 1
     fi
 
+    # Check if base virtual machine already exists
+    output=$(python3 vm_manager.py list)
+
+    if ! echo "$output" | grep -q "$BASE_VM_NAME"; then
+        printf "\e[33m*\e[0m ATTENTION: THE BASE VIRTUAL MACHINE \033[32m%s\033[0m DOES NOT EXIST, WAIT...\n" "$BASE_VM_NAME"
+    fi
 
 }
 
--- temp.sh
+++ temp.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-# Run the manager and capture the output
-output=$(python3 vm_manager.py list)
-
-# Check if the output contains a line with "SpiralVM"
-if echo "$output" | grep -q "SpiralVM"; then
-    printf "\e[32m*\e[0m 'SpiralVM' VM found. Continuing...\n"
-    
-    #
-    # Coloque seus comandos de continuação aqui
-    #
-    
-else
-    printf "\e[31m*\e[0m No 'SpiralVM' VM found. Aborting.\n"
-    # exit 1 # Descomente esta linha se desejar que o script pare aqui
+if ! echo "$output" | grep -q "$BASE_VM_NAME"; then
+    printf "\e[33m*\e[0m ATTENTION: THE BASE VIRTUAL MACHINE \033[32m%s\033[0m DOES NOT EXIST, WAIT...\n" "$BASE_VM_NAME"
 fi
