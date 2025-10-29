fcb801c1c4c935c552815be71f4ef54aec90972c
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-29 13:04:06 -0300

    update

--- temp.sh
+++ temp.sh
@@ -2,3 +2,8 @@ ssh -p 22 root@10.0.12.249 "mkdir /root/builder"
 scp -P 22 /etc/spawn/VM/builder/basevm.sh root@10.0.12.249:/root/builder
 scp -P 22 -r /etc/spawn/VM/systemd root@10.0.12.249:/root/builder
 ssh -p 22 root@10.0.12.249 "cd /root/builder && chmod +x basevm.sh && ./basevm.sh"
+
+
+
+PermitRootLogin yes
+PermitEmptyPasswords yes
