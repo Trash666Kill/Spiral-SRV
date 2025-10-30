fd147dcc68ee1d9870a80915e1fcc2d683d94286
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-29 21:33:42 -0300

    update

--- dep/spawn/VM/spawn.sh
+++ dep/spawn/VM/spawn.sh
@@ -106,8 +106,8 @@ basevm() {
         waitobj 10.0.12.249 60 4 "$BASE_VM_NAME"
         #
         ssh -p 22 root@10.0.12.249 "mkdir /root/builder"
-        scp -P 22 /etc/spawn/VM/builder/basevm.sh root@10.0.12.249:/root/builder
-        scp -P 22 -r /etc/spawn/VM/systemd root@10.0.12.249:/root/builder
+        scp -P 22 -q /etc/spawn/VM/builder/basevm.sh root@10.0.12.249:/root/builder
+        scp -P 22 -q -r /etc/spawn/VM/systemd root@10.0.12.249:/root/builder
         ssh -p 22 root@10.0.12.249 "cd /root/builder && chmod +x basevm.sh && ./basevm.sh"
 
     else
