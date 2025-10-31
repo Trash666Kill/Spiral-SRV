6d2f06e9876e7cfb2ec41e14df925706ce9b5d50
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-31 18:41:46 -0300

    update

--- temp.sh
+++ temp.sh
@@ -1,15 +1,44 @@
-    # Checks if the files needed to create the new virtual machine exist 
-    local missing_files=0 # Variable to count missing files
+# Inicia a criação da nova máquina vitual a partir da base
+    printf "\e[32m*\e[0m CREATING VIRTUAL MACHINE FROM BASE, WAIT...\n"
+    eval "$VM_MANAGER" copy "$BASE_VM_NAME" "$NEW_VM_NAME"
 
-    for file in "${NEW_VM_FILES[@]}"; do
-        if [[ ! -f "$file" ]]; then
-            printf "\e[31m*\e[0m ERROR: REQUIRED FILE DOES NOT EXIST: \033[32m%s\033[0m\n" "$file"
-            missing_files=$((missing_files + 1)) # Increment the counter
-        fi
-    done
+    reserve() {
+    # Obtém o endereço IP do virtual machine a partir do DNS
+    local IP_ADDRESS=$(/etc/spawn/grepip.sh)
 
-    # If any file was missing, exit the script
-    if [[ $missing_files -gt 0 ]]; then
-        printf "\e[31m*\e[0m ERROR: %d REQUIRED FILE(S) MISSING. ABORTING.\n" "$missing_files"
-        exit 1
-    fi
+    # Monta a string de reserva de DNS
+    RESULT="$MAC_ADDRESS,$IP_ADDRESS,$NEW_VM_NAME"
+    printf "\033[32m*\033[0m IP ADDRESS FIXED.\n"
+
+    # Modifica a configuração do dnsmasq para adicionar a reserva de IP
+    kill -SIGHUP $(pidof dnsmasq)
+    echo "$RESULT" >> /etc/dnsmasq.d/config/reservations
+    kill -SIGHUP $(pidof dnsmasq)
+    }
+
+    # Pergunta ao usuário se o endereço de IP deve ser reservado
+    read -p "WANT TO RESERVE THE NEXT AVAILABLE IP [y/n]? " x
+    case "$x" in
+        y)
+            reserve  # Chama a função para coletar o próximo endereço de IP válido e fixa-o
+            ;;
+        n)
+            printf "\033[33m*\033[0m ATTENTION: A DYNAMIC IP ADDRESS WILL BE ASSIGNED TO THE VIRTUAL machine\n"
+            ;;
+        *)
+            printf "\033[31m*\033[0m ERROR: INVALID CHOICE, TYPE \033[32m'y'\033[0m IF YOU WANT TO FIX AN IP ADDRESS IN THE VIRTUAL machine AND \033[32m'n'\033[0m IF YOU PREFER TO LEAVE IT DYNAMIC\n"
+            ;;
+        esac
+
+    # Inicia o novo virtual machine
+    printf "\033[32m*\033[0m STARTING...\n"
+    cp later.sh /var/lib/lxc/"${NEW_CT}"/rootfs/root/
+    lxc-start --name "${NEW_CT}"
+
+    # Torna o script later.sh executável e o executa dentro do virtual machine
+    lxc-attach --name "${NEW_CT}" -- chmod +x /root/later.sh
+    lxc-attach --name "${NEW_CT}" -- /root/later.sh
+else
+    printf "\e[31m*\e[0m ERROR CREATING VIRTUAL MACHINE \033[32m%s\033[0m.\n" "$NEW_CT"
+    exit 1
+fi
