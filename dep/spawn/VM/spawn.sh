1e2caf8571ad97be6b1ec0e71e7927cc1acb9189
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-10-31 17:44:36 -0300

    update

--- dep/spawn/VM/spawn.sh
+++ dep/spawn/VM/spawn.sh
@@ -76,6 +76,25 @@ waitobj() {
     fi
 }
 
+newvm() {
+    # Checks if the files needed to create the new virtual machine exist 
+    local missing_files=0 # Variable to count missing files
+
+    for file in "${NEW_VM_FILES[@]}"; do
+        if [[ ! -f "$file" ]]; then
+            printf "\e[31m*\e[0m ERROR: REQUIRED FILE DOES NOT EXIST: \033[32m%s\033[0m\n" "$file"
+            missing_files=$((missing_files + 1)) # Increment the counter
+        fi
+    done
+
+    # If any file was missing, exit the script
+    if [[ $missing_files -gt 0 ]]; then
+        printf "\e[31m*\e[0m ERROR: %d REQUIRED FILE(S) MISSING. ABORTING.\n" "$missing_files"
+        exit 1
+    fi
+
+}
+
 basevm() {
     # Checks if the files needed to create the base virtual machine exist 
     local missing_files=0 # Variable to count missing files
@@ -110,28 +129,14 @@ basevm() {
         scp -P 22 -q /etc/spawn/VM/builder/basevm.sh root@10.0.12.249:/root/builder
         scp -P 22 -q -r /etc/spawn/VM/systemd root@10.0.12.249:/root/builder
         ssh -p 22 -q root@10.0.12.249 "cd /root/builder && chmod +x basevm.sh && ./basevm.sh"
+        # Chamada para a função responsável pela criação no novo convidado baseado na base
+        newvm
 
     else
         printf "\e[32m*\e[0m INFO: A VM base \033[32m%s\033[0m já existe. Pulando criação.\n" "$BASE_VM_NAME"
         echo "criando nova vm..."
+        newvm
     fi
 }
 
-newvm() {
-    # Checks if the files needed to create the new virtual machine exist 
-    local missing_files=0 # Variable to count missing files
-
-    for file in "${NEW_VM_FILES[@]}"; do
-        if [[ ! -f "$file" ]]; then
-            printf "\e[31m*\e[0m ERROR: REQUIRED FILE DOES NOT EXIST: \033[32m%s\033[0m\n" "$file"
-            missing_files=$((missing_files + 1)) # Increment the counter
-        fi
-    done
-
-    # If any file was missing, exit the script
-    if [[ $missing_files -gt 0 ]]; then
-        printf "\e[31m*\e[0m ERROR: %d REQUIRED FILE(S) MISSING. ABORTING.\n" "$missing_files"
-        exit 1
-    fi
-
-}
+#
