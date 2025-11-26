22af9d5207b22cffaecd5f942262a8f7b8c18b5b
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-11-26 18:01:17 -0300

    new and update

--- dep/spawn/CT/grepip.sh
+++ dep/spawn/CT/grepip.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
-# Disable bash history
-unset HISTFILE
+HOSTNAME_ARG="$1"
 
+if [ -z "$HOSTNAME_ARG" ]; then
+    echo "Erro: Hostname não fornecido." >&2
+    exit 1
+fi
+
+HOSTNAME_LOWER="${HOSTNAME_ARG,,}"
+
+# Configurações
 LEASES_FILE="/var/lib/misc/dnsmasq.leases"
 RESERVATIONS_FILE="/etc/dnsmasq.d/config/reservations"
-IP_RANGE="10.0.10"
+IP_RANGE_PREFIX="10.0.10"
+MAX_LIMIT=248  # Ajustado para bater com seu dhcp-range
 
-# Função para verificar se o IP está em uso em qualquer um dos arquivos
 is_ip_in_use() {
-local IP=$1
-grep -q "$IP" "$LEASES_FILE" || grep -q "$IP" "$RESERVATIONS_FILE"
+    local IP=$1
+    grep -Fw -q "$IP" "$LEASES_FILE" || grep -Fw -q "$IP" "$RESERVATIONS_FILE"
 }
 
-# Loop para encontrar o IP disponível mais próximo do '1'
-for i in {2..254}; do
-    CANDIDATE_IP="$IP_RANGE.$i"
+if [[ "$HOSTNAME_LOWER" == ct* ]]; then
+    START=3
+    STEP=2
+elif [[ "$HOSTNAME_LOWER" == vm* ]]; then
+    START=2
+    STEP=2
+else
+    START=2
+    STEP=1
+fi
+
+# O loop agora vai somente até a variável MAX_LIMIT (248)
+for ((i=START; i<=MAX_LIMIT; i+=STEP)); do
+    CANDIDATE_IP="$IP_RANGE_PREFIX.$i"
+    
     if ! is_ip_in_use "$CANDIDATE_IP"; then
         echo "$CANDIDATE_IP"
         exit 0
     fi
 done
 
-printf "\033[31m*\033[0m ERROR: NO AVAILABLE ADDRESSES FOUND IN RANGE: \033[32m%s\033[0m.\n" "$IP_RANGE"
+printf "\033[31m*\033[0m ERROR: NO AVAILABLE ADDRESSES FOUND FOR \033[33m%s\033[0m IN RANGE: \033[32m%s.2-%s\033[0m.\n" "$HOSTNAME_ARG" "$IP_RANGE_PREFIX" "$MAX_LIMIT" >&2
 exit 1
